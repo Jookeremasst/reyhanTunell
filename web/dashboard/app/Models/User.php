d4127e7d221b8faf01f84b4291163d2cb5d679e9
Jookeremasst <118732440+Jookeremasst@users.noreply.github.com>
2026-08-29 20:11:38 +0330

    Support username authentication for dashboard

--- web/dashboard/app/Models/User.php
+++ web/dashboard/app/Models/User.php
@@ -9,7 +9,7 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['name', 'username', 'email', 'password'];
 
     protected $hidden = ['password', 'remember_token'];
 
