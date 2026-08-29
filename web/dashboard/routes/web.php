cd7f52882c8bb94ddf67d0c6ae10eabf8977afaf
Jookeremasst <118732440+Jookeremasst@users.noreply.github.com>
2026-08-29 20:05:11 +0330

    Protect dashboard routes with authentication

--- web/dashboard/routes/web.php
+++ web/dashboard/routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'create'])->name('login');
+    Route::post('/login', [LoginController::class, 'store'])->middleware('throttle:login')->name('login.store');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
