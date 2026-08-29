d3b8ee8640fa845b0511c4f77f5ac904a8a7ae27
Jookeremasst <118732440+Jookeremasst@users.noreply.github.com>
2026-08-29 20:05:16 +0330

    Add login throttling without custom limiter configuration

--- web/dashboard/routes/web.php
+++ web/dashboard/routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'create'])->name('login');
-    Route::post('/login', [LoginController::class, 'store'])->middleware('throttle:login')->name('login.store');
+    Route::post('/login', [LoginController::class, 'store'])->middleware('throttle:6,1')->name('login.store');
 });
 
 Route::middleware('auth')->group(function () {
